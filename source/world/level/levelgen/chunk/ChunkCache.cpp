c534ba732274fd57270443f474b66bb1cfc6b929
Blizzardfur <111100393+Blizzardfur-Maxxx@users.noreply.github.com>
2025-08-06 09:03:31 -0500

    fix saveUnsaved

--- source/world/level/levelgen/chunk/ChunkCache.cpp
+++ source/world/level/levelgen/chunk/ChunkCache.cpp
@@ -386,16 +386,13 @@ void ChunkCache::saveUnsaved()
 	std::vector<LevelChunk*> chunksToSave;
 
 #ifdef INFWORLDS
-	for (int i = 0; i < CHUNK_CACHE_WIDTH; i++)
-	{
-		for (int j = 0; j < CHUNK_CACHE_WIDTH; j++)
-		{
-			LevelChunk* pChunk = m_chunkMap[i][j];
-			if (pChunk && pChunk != m_pEmptyChunk && pChunk->m_bUnsaved)
-			{
-				chunksToSave.push_back(pChunk);
-			}
-		}
+	for (int i = 0; i < CHUNK_CACHE_WIDTH; i++) {
+	    for (int j = 0; j < CHUNK_CACHE_WIDTH; j++) {
+	        LevelChunk* pChunk = m_chunkMap[i][j];
+	        if (pChunk && pChunk != m_pEmptyChunk && pChunk->shouldSave(true)) {
+	            chunksToSave.push_back(pChunk);
+	        }
+	    }
 	}
 #else
 	for (int i = 0; i < C_MAX_CHUNKS_Z; i++)
